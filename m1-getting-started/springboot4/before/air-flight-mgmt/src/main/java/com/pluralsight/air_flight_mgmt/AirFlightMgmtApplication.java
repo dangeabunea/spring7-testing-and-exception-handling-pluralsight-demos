8db12307381b4cfcbe5b71d127140fac4ac70590
Dan Geabunea <dangeabunea@hotmail.com>
2026-01-12 12:18:33 +0200

    Demo 2 - Spring Boot Test Config

---
+++ m1-getting-started/springboot4/before/air-flight-mgmt/src/main/java/com/pluralsight/air_flight_mgmt/AirFlightMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.air_flight_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirFlightMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirFlightMgmtApplication.class, args);
+	}
+
+}
---
+++ m1-getting-started/springboot4/complete/air-flight-mgmt/src/test/java/com/pluralsight/air_flight_mgmt/AirFlightMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.air_flight_mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirFlightMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
