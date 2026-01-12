140b11285d15c852c30d24e13b282b8dfd7ec6ce
Dan Geabunea <dangeabunea@hotmail.com>
2026-01-12 12:36:23 +0200

    Finished demo 3 for module 1

--- m1-getting-started/springboot4/complete/air-flight-mgmt/src/main/java/com/pluralsight/afm/AirFlightMgmtApplication.java
+++ m1-getting-started/springboot4/complete/air-flight-mgmt/src/main/java/com/pluralsight/afm/AirFlightMgmtApplication.java
@@ -1,4 +1,4 @@
-package com.pluralsight.air_flight_mgmt;
+package com.pluralsight.afm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- m1-getting-started/springboot4/complete/air-flight-mgmt/src/test/java/com/pluralsight/afm/AirFlightMgmtApplicationTests.java
+++ m1-getting-started/springboot4/complete/air-flight-mgmt/src/test/java/com/pluralsight/afm/AirFlightMgmtApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pluralsight.air_flight_mgmt;
+package com.pluralsight.afm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
