910e15b6317dd454d5fde7f338e00775ac553a30
Dan Geabunea <dangeabunea@hotmail.com>
2026-02-05 19:18:30 +0200

    Preparing deps for m2

--- m2-integration-testing/before/air-flight-mgmt/src/test/java/com/pluralsight/afm/controller/FlightControllerFullIntegrationTest.java
+++ m2-integration-testing/before/air-flight-mgmt/src/test/java/com/pluralsight/afm/controller/FlightControllerFullIntegrationTest.java
@@ -1,5 +1,14 @@
 package com.pluralsight.afm.controller;
 
+import com.pluralsight.afm.repository.FlightRepository;
+import com.pluralsight.afm.service.AlertService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.client.RestTestClient;
+
 /**
  * Full integration test for FlightController using @SpringBootTest.
  *
@@ -12,7 +21,5 @@ package com.pluralsight.afm.controller;
  * - Slower startup (~5-6 seconds) but tests real component interactions
  * - Data persists across tests - use @BeforeEach cleanup for test isolation
  */
-
 class FlightControllerFullIntegrationTest {
-
 }
