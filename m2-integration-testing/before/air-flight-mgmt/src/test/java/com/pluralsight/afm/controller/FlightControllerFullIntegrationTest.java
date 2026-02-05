11a699449edd29c3041aec369041ab06f7d51423
Dan Geabunea <dangeabunea@hotmail.com>
2026-02-05 19:26:13 +0200

    More test deps

--- m2-integration-testing/before/air-flight-mgmt/src/test/java/com/pluralsight/afm/controller/FlightControllerFullIntegrationTest.java
+++ m2-integration-testing/before/air-flight-mgmt/src/test/java/com/pluralsight/afm/controller/FlightControllerFullIntegrationTest.java
@@ -1,13 +1,27 @@
 package com.pluralsight.afm.controller;
 
+import com.pluralsight.afm.domain.Flight;
+import com.pluralsight.afm.domain.FlightStatus;
+import com.pluralsight.afm.dto.CreateFlightDto;
 import com.pluralsight.afm.repository.FlightRepository;
 import com.pluralsight.afm.service.AlertService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.client.RestTestClient;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 /**
  * Full integration test for FlightController using @SpringBootTest.
