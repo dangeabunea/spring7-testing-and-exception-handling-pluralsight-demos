c0ac8545905295fb990dce59cd420270f56cabaf
Dan Geabunea <dangeabunea@hotmail.com>
2026-02-05 19:56:34 +0200

    More poreps for jpa test

--- m2-integration-testing/before/air-flight-mgmt/src/test/java/com/pluralsight/afm/repository/FlightRepositoryIntegrationTest.java
+++ m2-integration-testing/before/air-flight-mgmt/src/test/java/com/pluralsight/afm/repository/FlightRepositoryIntegrationTest.java
@@ -1,6 +1,16 @@
 package com.pluralsight.afm.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.pluralsight.afm.domain.Flight;
+import com.pluralsight.afm.domain.FlightStatus;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class FlightRepositoryIntegrationTest {
 
